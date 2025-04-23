401f559838d845ada2d546b4eaedcf9d393a7dc5
eliakip <kiplagateliakim52@gmail.com>
2025-04-23 10:52:40 +0300

    Update antilinks.js

--- plugins/antilinks.js
+++ plugins/antilinks.js
@@ -66,7 +66,7 @@ cmd({
       if (warningCount < 4) {
         // Send warning message
         await conn.sendMessage(from, {
-          text: `‎*⚠️ CASEYRHODES XMD LINKS NOT ALLOWED ⚠️*\n` +
+          text: `‎*⚠️ ELIAKIM XMD LINKS NOT ALLOWED ⚠️*\n` +
                 `*╭────⬡ WARNING ⬡────*\n` +
                 `*├▢ USER :* @${sender.split('@')[0]}!\n` +
                 `*├▢ COUNT : ${warningCount}*\n` +
